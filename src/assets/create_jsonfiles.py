8c37d944b234e0a9f42f41c4a910821d3d5fd02c
Dirk Frerichs <dirk.frerichs@dart-racing.de>
2020-03-22 14:00:27 +0100

    inital commit of creating fake data with example

---
+++ src/assets/create_jsonfiles.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Dies ist eine temporäre Skriptdatei.
+"""
+
+import json
+import datetime,time
+import random
+
+event={} # inital event
+
+today = datetime.date.today()
+first = today.replace(day=6)
+lastMonth = first - datetime.timedelta(days=6) 
+timestamp = time.mktime(lastMonth.timetuple())
+even_name=["Yoga","Meditieren","Frühstück","30-Min Trimm dich"]
+
+event_video=["skype","zoom","whatspp","n/a","discord","telegram"]
+
+event["category"] = "category"
+event["name"] = "test"
+event["description"] = "hier ist beschreibung"
+event["when"] = timestamp
+event["link"] = "www.chefkoch.de"
+event["video"] = event_video[0]
+event["phone"] = "+49 49 18880"
+event["phonepin"] = "18880"
+
+
+with open('data_example.json', 'w') as outfile:
+    json.dump(event, outfile)
